5cb706c45e4adb7d9db7c21e9331c479da53d8b7
Stephen <stephen@klu.ai>
2026-07-24 15:50:10 +0700

    Export the canonical management client

--- packages/management-client/src/index.ts
+++ packages/management-client/src/index.ts
@@ -24,11 +24,33 @@ export {
 	type AnySchema,
 	type OperationInput,
 	type OperationOutput,
+	type PathProjection,
 	type OperationRegistry,
 	type OperationSpec,
 } from "./spec.js";
 export { createOperationResource } from "./resources/operations.js";
 export { createOrganizationMemberResource } from "./resources/organizations.js";
+export {
+	assembleManagementOperationRegistry,
+	type OperationSchemaDomain,
+	type OperationSchemaPair,
+} from "./generated/assemble.js";
+export { OPERATION_METADATA, type OperationMetadata } from "./generated/operation-metadata.js";
+export {
+	MANAGEMENT_OPERATION_REGISTRY,
+	type ManagementOperationRegistry,
+} from "./generated/registry.js";
+export { SYSTEM_OPERATION_SCHEMAS } from "./generated/system.js";
+export { PROJECT_OPERATION_SCHEMAS } from "./generated/projects.js";
+export { ENVIRONMENT_OPERATION_SCHEMAS } from "./generated/environments.js";
+export { EVENTS_IDENTITY_OPERATION_SCHEMAS } from "./generated/events-identity.js";
+export { AUTHORIZATION_OPERATION_SCHEMAS } from "./generated/authorization.js";
+export { ENTERPRISE_OPERATION_SCHEMAS } from "./generated/enterprise.js";
+export { DELIVERY_OPERATION_SCHEMAS } from "./generated/delivery.js";
+export { POLICY_CONFIG_OPERATION_SCHEMAS } from "./generated/policy-config.js";
+export { DATA_OPERATION_SCHEMAS } from "./generated/data-operations.js";
+export { SCHEMA_OPERATION_SCHEMAS } from "./generated/schema-operations.js";
+export { RESOURCE_OPERATION_SCHEMAS } from "./generated/resources.js";
 export {
 	ORGANIZATION_MEMBER_FIXTURE_OPERATIONS,
 	ORGANIZATION_MEMBER_REMOVE,
